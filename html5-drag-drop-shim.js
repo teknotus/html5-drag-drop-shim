879a9b95915656f253aea0426b2f49dc2654c483
Daniel Patrick Johnson <teknotus@gmail.com>
2013-08-16 00:34:22 -0700

    Converted to one big object

--- html5-drag-drop-shim.js
+++ html5-drag-drop-shim.js
@@ -1,15 +1,18 @@
-console.log("adding drag drop event listeners");
-
-function DataTransfer() {
-  this.types = [];
-  this.data = {};
-  this.dragElement;
-  this.dragImage;
-  this.dropEffect = 'none';
-  this.effectAllowed = 'uninitialized';
-  this.effectAllowedMask = 7;
+var DragAndDrop = function(){
+  this.dragging = false;
+  this.nodes = [];
+  this.dataTransfer;
 };
-DataTransfer.prototype = {
+DragAndDrop.prototype.DataTransfer = function(){
+    this.types = [];
+    this.data = {};
+    this.dragElement;
+    this.dragImage;
+    this.dropEffect = 'none';
+    this.effectAllowed = 'uninitialized';
+    this.effectAllowedMask = 7;
+  };
+DragAndDrop.prototype.DataTransfer.prototype = {
   setData: function(type, value){
     this.types.push(type);
     this.data[type] = value;
@@ -27,7 +30,7 @@ DataTransfer.prototype = {
   setDragImage: function(image,x,y){
     this.dragImage = {
       image: image,
-      clone: cloneWithStyle(image),
+      clone: this.cloneWithStyle(image),
       x: x,
       y: y
     };
@@ -76,15 +79,59 @@ DataTransfer.prototype = {
     } else {
       this._dropEffect = 'none';
     }
+  },
+  copyStyle: function(original,copy){
+    var originalStyle = window.getComputedStyle(original);
+    var bodyStyle = window.getComputedStyle(document.body);
+    var key;
+    for(var l = originalStyle.length,i = 0; i < l ; i++){
+      key = originalStyle[i];
+      if(originalStyle[key] != bodyStyle[key] || key == 'outline'){
+        // only copy styles not inherited from the body
+        copy.style[key] = originalStyle[key];
+      }
+    };
+  },
+  cloneWithStyle: function(nodeOrig){
+    var copyRoot = document.createElement('div');
+    var treeWalker = document.createTreeWalker(
+      nodeOrig, NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT, null, false);
+    var node = treeWalker;
+    var currentNode,newNode;
+    var copyNode = copyRoot;
+    var appendType = 'child';
+    var explode = 5000;
+    while(explode > 0){
+      explode--;
+      currentNode = treeWalker.currentNode;
+      if(currentNode.nodeType == 1){
+        newNode = document.createElement(currentNode.nodeName);
+        this.copyStyle(currentNode,newNode);
+      }else if(currentNode.nodeType == 3){
+        newNode = document.createTextNode(currentNode.nodeValue);
+      }
+      if(appendType === 'child'){
+        copyNode.appendChild(newNode);
+      } else if(appendType === 'sibling'){
+        copyNode.parentNode.appendChild(newNode);
+      }
+      copyNode = newNode;
+      if(node.firstChild()){
+        appendType = 'child';
+      } else {
+        while(!node.nextSibling()){
+          if(node.currentNode == node.root){
+            return copyRoot;
+          }
+          node.parentNode();
+          copyNode = copyNode.parentNode;
+        }
+        appendType = 'sibling';
+      }
+    }
   }
 };
-
-var drag_and_drop = {
-  dragging: false,
-  nodes: [],
-  dataTransfer: new DataTransfer()
-};
-findElementNodes = function(node){
+DragAndDrop.prototype.findElementNodes = function(node){
   var body = document.body;
   var nodes = [];
   while(node != document) {
@@ -100,7 +147,7 @@ findElementNodes = function(node){
   }
   return nodes.reverse();
 };
-findDraggableNodes = function(node){
+DragAndDrop.prototype.findDraggableNodes = function(node){
   // could do this with xpath
   // './/ancestor-or-self::*[@draggable="true"]'
   var draggable = 'dragger';
@@ -119,57 +166,7 @@ findDraggableNodes = function(node){
     }
     return draggable_nodes;
 };
-copyStyle = function(original,copy){
-  var originalStyle = window.getComputedStyle(original);
-  var bodyStyle = window.getComputedStyle(document.body);
-  var key;
-  for(var l = originalStyle.length,i = 0; i < l ; i++){
-    key = originalStyle[i];
-    if(originalStyle[key] != bodyStyle[key] || key == 'outline'){
-      // only copy styles not inherited from the body
-      copy.style[key] = originalStyle[key];
-    }
-  };
-}
-cloneWithStyle = function(nodeOrig){
-  var copyRoot = document.createElement('div');
-  var treeWalker = document.createTreeWalker(
-    nodeOrig, NodeFilter.SHOW_ELEMENT | NodeFilter.SHOW_TEXT, null, false);
-  var node = treeWalker;
-  var currentNode,newNode;
-  var copyNode = copyRoot;
-  var appendType = 'child';
-  var explode = 5000;
-  while(explode > 0){
-    explode--;
-    currentNode = treeWalker.currentNode;
-    if(currentNode.nodeType == 1){
-      newNode = document.createElement(currentNode.nodeName);
-      copyStyle(currentNode,newNode);
-    }else if(currentNode.nodeType == 3){
-      newNode = document.createTextNode(currentNode.nodeValue);
-    }
-    if(appendType === 'child'){
-      copyNode.appendChild(newNode);
-    } else if(appendType === 'sibling'){
-      copyNode.parentNode.appendChild(newNode);
-    }
-    copyNode = newNode;
-    if(node.firstChild()){
-      appendType = 'child';
-    } else {
-      while(!node.nextSibling()){
-        if(node.currentNode == node.root){
-          return copyRoot;
-        }
-        node.parentNode();
-        copyNode = copyNode.parentNode;
-      }
-      appendType = 'sibling';
-    }
-  }
-}
-touchStartCallback = function(e){
+DragAndDrop.prototype.touchStartCallback = function(e){
   var target,clientX,clientY;
   if(e.type == 'mousedown'){
     console.log("mousedown");
@@ -185,28 +182,28 @@ touchStartCallback = function(e){
       clientY = e.touches[0].clientY;
     }
   }
-  var dnodes = findDraggableNodes(target);
+  var dnodes = this.findDraggableNodes(target);
   var dcount = dnodes.length;
 //  console.log('draggable_nodes count: ' + dcount);
   if(dcount){
     e.preventDefault();
-    drag_and_drop.dragging = true;
-    drag_and_drop.dragNode = dnodes[0];
-    drag_and_drop.nodes = findElementNodes(target);
+    this.dragging = true;
+    this.dragNode = dnodes[0];
+    this.nodes = this.findElementNodes(target);
 
     var dragStartEvent = document.createEvent("Event");
     dragStartEvent.initEvent("dragstart", true, true);
-    var dataTransfer = new DataTransfer();
-    dragStartEvent.dataTransfer = drag_and_drop.dataTransfer = dataTransfer;
+    var dataTransfer = new this.DataTransfer();
+    dragStartEvent.dataTransfer = this.dataTransfer = dataTransfer;
     var dragStartCanceled = !dnodes[0].dispatchEvent(dragStartEvent);
 
     var rect = dnodes[0].getBoundingClientRect();
     var offsetY = clientY - rect.top;
     var offsetX = clientX - rect.left;
-    drag_and_drop['rect'] = rect;
-    drag_and_drop['offset'] = { X: offsetX, Y: offsetY };
-    drag_and_drop.dataTransfer.setDragImage(dnodes[0],offsetX,offsetY);
-    var dcopy = drag_and_drop.dataTransfer.dragImage.clone;
+    this['rect'] = rect;
+    this['offset'] = { X: offsetX, Y: offsetY };
+    this.dataTransfer.setDragImage(dnodes[0],offsetX,offsetY);
+    var dcopy = this.dataTransfer.dragImage.clone;
     document.body.appendChild(dcopy);
     dcopy.style.position = "absolute";
     dcopy.style['z-index'] = 5000;
@@ -217,28 +214,28 @@ touchStartCallback = function(e){
     var Y = clientY - offsetY;
     dcopy.style['-webkit-transform'] = 'translate(' + X + 'px, ' + Y + 'px)';
     dcopy.style['pointer-events'] = 'none';
-    drag_and_drop.dcopy = dcopy;
+    this.dcopy = dcopy;
     var copyOrigRect = dcopy.getBoundingClientRect();
-    drag_and_drop['copyOrigRect'] = copyOrigRect;
+    this['copyOrigRect'] = copyOrigRect;
 
   }
 }
-touchEndCallback = function(e){
+DragAndDrop.prototype.touchEndCallback = function(e){
   console.log("mouseup or touchend");
-  drag_and_drop.dragging = false;
-  document.body.removeChild(drag_and_drop.dcopy);
-  var dropElement = drag_and_drop.dropElement;
+  this.dragging = false;
+  document.body.removeChild(this.dcopy);
+  var dropElement = this.dropElement;
   if(dropElement !== null){
     var dropEvent = document.createEvent("Event");
     dropEvent.initEvent("drop", true, true);
-    dropEvent.dataTransfer = drag_and_drop.dataTransfer;
-    dropEvent.clientX = drag_and_drop.moveData.clientX;
-    dropEvent.clientY = drag_and_drop.moveData.clientY;
+    dropEvent.dataTransfer = this.dataTransfer;
+    dropEvent.clientX = this.moveData.clientX;
+    dropEvent.clientY = this.moveData.clientY;
     dropElement.dispatchEvent(dropEvent);
   } else {
     console.log('invalid drop target');
   }
-  var oldNodes = drag_and_drop.nodes;
+  var oldNodes = this.nodes;
   for(var on = oldNodes.length -1 ; on >= 0 ; on--){
     var dragLeaveEvent = document.createEvent("Event");
     dragLeaveEvent.initEvent("dragleave", true, true);
@@ -246,12 +243,12 @@ touchEndCallback = function(e){
   }
   var dragEndEvent = document.createEvent("Event");
   dragEndEvent.initEvent("dragend", true, true);
-  dragEndEvent.screenX = drag_and_drop.moveData.screenX;
-  dragEndEvent.screenY = drag_and_drop.moveData.screenY;
-  drag_and_drop.dragNode.dispatchEvent(dragEndEvent);
+  dragEndEvent.screenX = this.moveData.screenX;
+  dragEndEvent.screenY = this.moveData.screenY;
+  this.dragNode.dispatchEvent(dragEndEvent);
 }
-touchMoveCallback = function(e){
-  if(drag_and_drop.dragging){
+DragAndDrop.prototype.touchMoveCallback = function(e){
+  if(this.dragging){
     console.log('dragging');
     var moveData = {}
     if(e.type == 'mousemove'){
@@ -271,21 +268,21 @@ touchMoveCallback = function(e){
         moveData.screenY = e.touches[0].screenY;
       }
     }
-    drag_and_drop.moveData = moveData;
+    this.moveData = moveData;
     var element = document.elementFromPoint(moveData.clientX,moveData.clientY);
-    drag_and_drop.dropElement = element;
+    this.dropElement = element;
     if(element !== null){
       var dragOverEvent = document.createEvent("Event");
       dragOverEvent.initEvent("dragover", true, true);
-      dragOverEvent.dataTransfer = drag_and_drop.dataTransfer;
+      dragOverEvent.dataTransfer = this.dataTransfer;
       var dragOverCanceled = !element.dispatchEvent(dragOverEvent);
     }
-    var oldNodes = drag_and_drop.nodes;
+    var oldNodes = this.nodes;
     var newNodes;
     if(element === null){
       newNodes = [];
     } else {
-      newNodes = findElementNodes(element);
+      newNodes = this.findElementNodes(element);
     }
     var elCount = (oldNodes.length < newNodes.length) ?
       oldNodes.length : newNodes.length;
@@ -301,10 +298,10 @@ touchMoveCallback = function(e){
       dragEnterEvent.initEvent("dragenter", true, true);
       newNodes[i].dispatchEvent(dragEnterEvent);
     }
-    drag_and_drop.nodes = newNodes;
-    var dcopy = drag_and_drop.dcopy;
-    var offset = drag_and_drop.offset;
-    var copyOrigWidth = drag_and_drop.copyOrigRect.width;
+    this.nodes = newNodes;
+    var dcopy = this.dcopy;
+    var offset = this.offset;
+    var copyOrigWidth = this.copyOrigRect.width;
     var X = moveData.clientX - offset.X;
     var Y = moveData.clientY - offset.Y;
     // if dragged item extends to right of screen trim it to fit
@@ -317,10 +314,13 @@ touchMoveCallback = function(e){
     dcopy.style['-webkit-transform'] = 'translate(' + X + 'px, ' + Y + 'px)';
   }
 }
-document.addEventListener('mousedown', touchStartCallback, true);
-document.addEventListener('touchstart', touchStartCallback, true);
-document.addEventListener('mouseup', touchEndCallback, true);
-document.addEventListener('touchend', touchEndCallback, true);
-document.addEventListener('mousemove', touchMoveCallback, true);
-document.addEventListener('touchmove', touchMoveCallback, true);
+
+console.log("adding drag drop event listeners");
+var dnd = new DragAndDrop();
+document.addEventListener('mousedown', dnd.touchStartCallback.bind(dnd), true);
+document.addEventListener('touchstart', dnd.touchStartCallback.bind(dnd), true);
+document.addEventListener('mouseup', dnd.touchEndCallback.bind(dnd), true);
+document.addEventListener('touchend', dnd.touchEndCallback.bind(dnd), true);
+document.addEventListener('mousemove', dnd.touchMoveCallback.bind(dnd), true);
+document.addEventListener('touchmove', dnd.touchMoveCallback.bind(dnd), true);
 
